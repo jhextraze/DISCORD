c3b0211bfb1161fb63ada6cd1c18b59857d4d960
Jhexter Valencia <rjhextervalencia@gmail.com>
2025-03-06 07:46:07 +0800

    FINAL NA TO

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,17 @@
-const headerNav = document.querySelector(".header-nav");
-const burgerBtn = document.querySelector(".burger-btn-real");
+const headerNav = document.querySelector(".header__nav");
+const burgerBtn = document.querySelector(".burgerBtn");
 burgerBtn.addEventListener("click", () => {
     headerNav.classList.toggle("open");
     burgerBtn.classList.toggle("open");
-});
+})
+
+const closeBtn = document.querySelector(".header__nav__closeBtn i");
+closeBtn.addEventListener("click", () => {
+    headerNav.classList.toggle("open");
+    burgerBtn.classList.toggle("open");
+})
+
+window.addEventListener('scroll', () => {
+    var header = document.querySelector('.header');
+    header.classList.toggle("sticky", window.scrollY > 100);
+})
